e4e7d6b04973f11750bb0eef313e0a6887c3b819
Tanmay Somani <tanmaysomani2003@gmail.com>
2022-11-28 20:02:44 +0530

    Add files via upload

--- Day7ex.py
+++ Day7ex.py
@@ -1,10 +1,11 @@
 names = ["Joseph", "Nathan", "Sasha", "Kelly",
-         "Muhammad", "Jabulani", "Sera", "Patel", "Sera"]
+         "Muhammad", "Jabulani", "Sera", "Patel", "Sera", "Sera", "Sasha"]
 ctr_val = 0
 dict = {}
 for i in names:
     if i[0] == "S":
-        alpha = i
-        ctr_val += 1
-        dict[alpha] = ctr_val
+        if i in dict:
+            dict[i] = ctr_val
+            ctr_val += 1
+        dict[i] = ctr_val+1
 print(dict)
