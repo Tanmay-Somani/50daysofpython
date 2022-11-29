017d85e6fa019ea649ab7611a6f0c1adb24cf5a5
Tanmay Somani <tanmaysomani2003@gmail.com>
2022-11-29 20:39:59 +0530

    Add files via upload
    incomplete too many errors

---
+++ Day8ex.py
@@ -0,0 +1,16 @@
+def prime_numbers():
+    list = [2]
+    stop = int(input("Insert the range of the integer"))
+    for i in range(2, stop):
+        for j in range(2, i):
+            if i % j == 0:
+                break
+            else:
+                if i in list:
+                    break
+                else:
+                    list.append(i)
+    return list
+
+
+print(prime_numbers())
