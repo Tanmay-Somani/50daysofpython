56126d0aacbca7498b9057c7c3d4910d79d9c35b
Tanmay Somani <tanmaysomani2003@gmail.com>
2022-12-01 20:50:09 +0530

    Add files via upload

---
+++ Day10.py
@@ -0,0 +1,9 @@
+def hide_password():
+    passw = input()
+    passw = "*"*len(passw)
+    print(passw)
+    print("the password is ", len(passw), "characters long")
+
+
+passw = " "
+hide_password()
