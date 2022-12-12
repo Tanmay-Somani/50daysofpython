2b5c05442e508edd53b3cd8ed1c41e8c6d020d75
Tanmay Somani <tanmaysomani2003@gmail.com>
2022-12-12 20:42:20 +0530

    Add files via upload

--- Day21ex.py
+++ Day21ex.py
@@ -1,14 +1,16 @@
 def even_or_average():
     user = []
     max = 0
-    sum = 0
+    avg = 0
     for i in range(5):
         user.append(int(input("Enter a number")))
         if max < user[i]:
             max = user[i]
-
-    if user[0] % 2 != 0 or user[1] % 2 != 0 or user[2] % 2 != 0 or user[3] % 2 != 0 or user[4] % 2 != 0:
-        avg = (user[0]+user[1]+user[2]+user[3]+user[4])/5
+    for i in range(5):
+        if user[i] % 2 != 0:
+            avg = (user[0]+user[1]+user[2]+user[3]+user[4])/5
+        else:
+            return max
         return avg
     return max
 
