51c99939c5ec2d1ff61aee59d8f651bb1cbd03ae
Tanmay Somani <tanmaysomani2003@gmail.com>
2023-07-31 00:37:39 +0530

    Update Day1.py

--- Day1.py
+++ Day1.py
@@ -3,12 +3,9 @@ def divide_or_square(n):
         return n**0.5
     else:
         return n//5
-
-
 def longest_value(x):
     max = x.keys()
     return max
-
 
 n = int(input())
 print(divide_or_square(n))
